9a1a05ff9cf14acdb4df8b0e563dcd79764ed037
eric <eric.basley@redpelicans.com>
2017-12-29 23:29:09 +0100

    Add a cat component that follow mouse

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { spring } from 'popmotion';
+import cat from './cat.svg';
 
 class WithMouse extends React.Component {
-  state = { x: 0, y: 0 }
+  state = { x: 0, y: 0 };
   handleMouseMove = event => {
     this.setState({
       x: event.clientX,
       y: event.clientY,
-    })
-  }
+    });
+  };
   render() {
-    const { children } = this.props
-    return <div onMouseMove={this.handleMouseMove}>{children(this.state)}</div>
+    const { children } = this.props;
+    return <div onMouseMove={this.handleMouseMove}>{children(this.state)}</div>;
   }
 }
 
 WithMouse.propTypes = {
   children: PropTypes.func.isRequired,
+};
+
+class Cat extends React.Component {
+  render() {
+    const { mouse } = this.props;
+    const style = {
+      top: mouse.y,
+      left: mouse.x,
+      position: 'fixed',
+      width: '50px',
+      height: '50px',
+    };
+    return <img style={style} alt="cat" src={cat} />;
+  }
 }
+
+Cat.propTypes = {
+  mouse: PropTypes.object.isRequired,
+};
 
 const App = () => (
   <WithMouse>
@@ -26,9 +46,11 @@ const App = () => (
         <h1>
           Mouse position is: ({x}, {y})
         </h1>
+        <Cat mouse={{ x, y }} />
       </div>
     )}
   </WithMouse>
-)
+);
 
-export default App
+spring({ from: { x: 200, y: 50 }, to: { x: 500, y: 100 } }).start(v => console.log(v));
+export default App;
