d5559b590ecd40eff9a442c0ddbd6a9137d8601b
eric <eric.basley@redpelicans.com>
2017-12-29 18:16:24 +0100

    Remove useless div

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React from 'react'
 
 class App extends React.Component {
-  state = { x: 0, y:0 }
+  state = { x: 0, y: 0 }
 
   handleMouseMove = event => {
     this.setState({
       x: event.clientX,
       y: event.clientY,
-    });
+    })
   }
 
-  render(){
-    const { x, y } = this.state;
+  render() {
+    const { x, y } = this.state
 
     return (
       <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
-        <div>
-          <h1>Mouse position is: ({x}, {y})</h1>
-        </div>
+        <h1>
+          Mouse position is: ({x}, {y})
+        </h1>
       </div>
-    ); 
+    )
   }
 }
 
-
-export default App;
+export default App
