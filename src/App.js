7781bb7df1ed1c9d3712b9b953883f3d75902cc2
eric <eric.basley@redpelicans.com>
2017-12-29 17:17:41 +0100

    Vanila version

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = { x: 0, y:0 }
+
+  handleMouseMove = event => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  }
+
+  render(){
+    const { x, y } = this.state;
+
+    return (
+      <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
+        <div>
+          <h1>Mouse position is: ({x}, {y})</h1>
+        </div>
+      </div>
+    ); 
+  }
+}
+
+
+export default App;
